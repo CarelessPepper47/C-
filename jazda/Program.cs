288f154a69c30f2179e737980cb4400bc6b74599
MatiDev <mtsz.raszeja@gmail.com>
2024-03-28 13:55:50 +0100

    dziala?

---
+++ jazda/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jazda
+{
+    internal class Program
+    {
+        static void Main(string[] args) // tu sie rozpoczyna execution
+        {
+            Console.Write("Dupa");
+            Console.Write("Dupa");
+            Console.WriteLine("");
+            /// cw <- a potem TAB x2 = Console.WriteLine();
+            Console.WriteLine("Dupa");
+            Console.WriteLine("Dupa");
+        }
+    }
+}
